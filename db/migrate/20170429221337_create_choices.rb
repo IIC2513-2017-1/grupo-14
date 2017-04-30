49056ef02a8ae0e64fa78d84f05d17700037794e
Rodancoci <rodancoci@gmail.com>
2017-04-30 16:26:40 -0300

    Fix user db validations (uniqueness)

--- db/migrate/20170429221337_create_choices.rb
+++ db/migrate/20170429221337_create_choices.rb
@@ -1,7 +1,7 @@
 class CreateChoices < ActiveRecord::Migration[5.0]
   def change
     create_table :choices do |t|
-      t.string :value
+      t.string :value, null: false
 
       t.timestamps
     end

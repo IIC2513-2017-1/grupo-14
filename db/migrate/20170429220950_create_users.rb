1d5db2ce2de0252c14b1c91a6e26faa6584e9ae6
Rodancoci <rodancoci@gmail.com>
2017-04-30 13:23:28 -0300

    User db validations

--- db/migrate/20170429220950_create_users.rb
+++ db/migrate/20170429220950_create_users.rb
@@ -1,10 +1,10 @@
 class CreateUsers < ActiveRecord::Migration[5.0]
   def change
     create_table :users do |t|
-      t.string :name
-      t.string :mail
-      t.string :password
-      t.string :role
+    t.string   "name", null: false
+    t.string   "mail", null: false
+    t.string   "password", null: false
+    t.string   "role", null: false
 
       t.timestamps
     end

c64fd450a66ee555a3981c7473816cc385ff2ae8
Rodancoci <rodancoci@gmail.com>
2017-04-30 18:45:52 -0300

    Participation db validations

--- db/migrate/20170430180042_create_participations.rb
+++ db/migrate/20170430180042_create_participations.rb
@@ -1,7 +1,7 @@
 class CreateParticipations < ActiveRecord::Migration[5.0]
   def change
     create_table :participations do |t|
-      t.integer :amount
+      t.integer :amount, null: false
 
       t.timestamps
     end

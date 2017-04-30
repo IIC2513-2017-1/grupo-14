910c9644a9c7729dad5f62241ea61983acf9407e
Rodancoci <rodancoci@gmail.com>
2017-04-30 14:21:20 -0300

    Fix user model validations, add user form validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
-	validates :password, presence: true, length: {minimum: 6}, confimation: true, allow_blank: false
-	validates :mail, presence: true, uniqueness: true, allow_blank: false, length: {minimum: 5}
+	validates :password, presence: true, length: {minimum: 6}, confirmation: true, allow_blank: false
+	validates :mail, presence: true, uniqueness: true, allow_blank: false,
 	format: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i
-	validates :role, inclusion: { in: %w(regular, admin, mod) }
+	validates :role, inclusion: { in: %w(regular admin mod) }
 	validates :name, presence: true, length: {minimum: 1}, allow_blank: false, uniqueness: true
 end

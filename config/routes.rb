b591203ee2c0f95d367380aa886497cc7c850294
Rodancoci <rodancoci@gmail.com>
2017-04-30 21:07:45 -0300

    Links to entity indexes

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,10 @@
 Rails.application.routes.draw do
   resources :bets
   resources :participations
-  get 'home/index'
-
   resources :choices
   resources :users
-
+  
+  get 'home/index'
   root 'home#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end

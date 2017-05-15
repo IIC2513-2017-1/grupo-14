339165bb218d4d309290d36a5f4135232ad66c18
Rodancoci <rodancoci@gmail.com>
2017-05-15 02:43:04 -0400

    Fighting with nested forms and bets

--- app/models/choice.rb
+++ app/models/choice.rb
@@ -2,6 +2,6 @@ class Choice < ApplicationRecord
 	validates :value, presence: true, length: { minimum: 6 }, allow_blank: false
 	validates_uniqueness_of :value, scope: [:bet_id]
 
-	belongs_to :bet
+	belongs_to :bet, optional: true
 	has_many :participation
 end

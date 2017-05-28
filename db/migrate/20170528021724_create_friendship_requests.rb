f1fc3afb271867f2b6cc154af3ebadfbb1200478
Rodancoci <rodancoci@gmail.com>
2017-05-28 03:33:01 -0400

    Friendship model & tables

---
+++ db/migrate/20170528021724_create_friendship_requests.rb
@@ -0,0 +1,11 @@
+class CreateFriendshipRequests < ActiveRecord::Migration[5.0]
+  def change
+    create_table :friendship_requests do |t|
+      t.integer :sender_id, null: false
+      t.integer :recipient_id, null: false
+
+      t.timestamps
+    end
+
+  end
+end

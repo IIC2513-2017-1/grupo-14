39b08e35a4622a4d9b0378835a183315fafd5aaf
Rodancoci <rodancoci@gmail.com>
2017-05-29 00:01:43 -0400

    Working friend requests

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,2 +1,17 @@
 module UsersHelper
+	def request_button(user)
+		return unless current_user
+		return if user == current_user
+		sent_request = user.incoming_requests.detect do |relation|
+			relation.sender_id == current_user.id
+		end
+		if sent_request
+			link_to 'Cancel friend request', friendship_request_path(sent_request), method: 'delete'
+		else
+			link_to 'Send friend request', user_friendship_requests_path(user), method: 'post'
+		end
+	end
+	def accept_friendship(user)
+		return unless current_user
+		return if user == current_user
 end

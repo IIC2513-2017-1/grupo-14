1fc7bc91b33197074eb31b8ad0c924a455274d0b
Rodancoci <rodancoci@gmail.com>
2017-05-29 02:08:14 -0400

    Finalize friend and friend request management

--- app/controllers/bets_controller.rb
+++ app/controllers/bets_controller.rb
@@ -1,4 +1,6 @@
 class BetsController < ApplicationController
+  include Secured
+  before_action :logged_in?, only: %i[new create edit update destroy]
   before_action :set_bet, only: [:show, :edit, :update, :destroy]
 
   # GET /bets
--- app/controllers/friendships_controller.rb
+++ app/controllers/friendships_controller.rb
@@ -12,15 +12,15 @@ class FriendshipsController < ApplicationController
   # POST /users.json
   def create
     user = User.find(params[:user_id])
-    friendship = current_user.friendships.build(friend: user.id)
-    if frienship.save
-      reciprocal_friendship = user.friendships.build(friend: current_user.id)
+    friendship = current_user.friendships.build(friend: user)
+    if friendship.save
+      reciprocal_friendship = user.friendships.build(friend: current_user)
       if reciprocal_friendship.save
         request_1 = FriendshipRequest.find_by(sender: current_user, recipient: user)
         request_2 = FriendshipRequest.find_by(sender: user, recipient: current_user)
         request_1.destroy if request_1
         request_2.destroy if request_2
-        redirect_to root_path, notice: "Friendship request accepted!"
+        redirect_to root_path, notice: "You and #{user.name} are now friends!"
       else
         friendship.destroy
         redirect_to root_path, notice: "There was an error creating the friendship"
@@ -35,13 +35,13 @@ class FriendshipsController < ApplicationController
   def destroy
     friendship = Friendship.find(params[:id])
     user = friendship.user
-    friend = frienship.friend
+    friend = friendship.friend
     reciprocal_friendship = Friendship.find_by(user: friend, friend: user)
-    frienship.destroy
+    friendship.destroy
     if reciprocal_friendship
       reciprocal_friendship.destroy
     end
-    redirect_to root_path, notice: "Friendship ended."
+    redirect_to root_path, notice: "Friendship with #{friend.name} ended."
   end
 
   private

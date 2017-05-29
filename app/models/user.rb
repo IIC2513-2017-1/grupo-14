b837b951dc78cd0054580dbc922521f65a38ce73
Rodancoci <rodancoci@gmail.com>
2017-05-29 05:30:54 -0400

    Add user avatars

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,14 +1,19 @@
 class User < ApplicationRecord
 	has_secure_password
+	mount_uploader :avatar, AvatarUploader
 
 	validates :password, presence: true, length: {minimum: 6}, confirmation: true, allow_blank: false
 	validates :mail, presence: true, uniqueness: true, allow_blank: false, 
 	format: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i
 	validates :role, inclusion: { in: %w(regular admin mod) }
 	validates :name, presence: true, length: {minimum: 1}, allow_blank: false, uniqueness: true
+
+	validate :valid_content_type, on: 'update'
 	
 	has_many :bets
 	has_many :participations
+
+	has_one :avatar
 
 	has_many :friendships, foreign_key: :user_id, class_name: 'Friendship'
 	has_many :friends, through: :friendships, source: :friend
@@ -17,5 +22,11 @@ class User < ApplicationRecord
 	has_many :outgoing_requests, foreign_key: :sender_id, class_name: 'FriendshipRequest'
 	has_many :request_targets, through: :outgoing_requests, source: :recipient
 	has_many :request_senders, through: :incoming_requests, source: :sender
+
+	private
+
+		def valid_content_type
+	      errors.add(:avatar, 'is not a valid image file') unless %w(image/jpeg image/png).include? avatar.sanitized_file.content_type
+	    end
 
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170528021724) do
+ActiveRecord::Schema.define(version: 20170529075322) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -73,6 +73,7 @@ ActiveRecord::Schema.define(version: 20170528021724) do
     t.datetime "created_at",      null: false
     t.datetime "updated_at",      null: false
     t.string   "password_digest"
+    t.string   "avatar"
     t.index ["mail"], name: "index_users_on_mail", unique: true, using: :btree
     t.index ["name"], name: "index_users_on_name", unique: true, using: :btree
   end

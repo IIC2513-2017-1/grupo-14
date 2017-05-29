fb941cf053bc3d013335d564240b03cbbda88e4d
Rodancoci <rodancoci@gmail.com>
2017-05-29 00:01:43 -0400

    Working friend requests

---
+++ app/controllers/friendship_requests_controller.rb
@@ -0,0 +1,36 @@
+class FriendshipRequestsController < ApplicationController
+  include Secured
+  before_action :set_friendship_request, only: [:destroy]
+  before_action :logged_in?
+
+  # GET /users/new
+  def new
+    @friendship_request = FriendshipRequest.new
+  end
+
+  # POST /users
+  # POST /users.json
+  def create
+    user = User.find(params[:user_id])
+    friend_request = user.incoming_requests.build(sender_id: current_user.id)
+    if friend_request.save
+      redirect_to root_path, notice: "Friend request sent to #{user.name}."
+    else
+      redirect_to root_path, alert: 'Friend request could not be sent.'
+    end
+  end
+
+  # DELETE /users/1
+  # DELETE /users/1.json
+  def destroy
+    request = FriendshipRequest.find(params[:id])
+    request.destroy
+    redirect_to root_path, notice: "Friend request to #{request.recipient.name} cancelled."
+  end
+
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_friendship_request
+      @friendship_request = FriendshipRequest.find(params[:id])
+    end
+end

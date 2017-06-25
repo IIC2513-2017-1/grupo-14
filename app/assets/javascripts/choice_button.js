1b0432b8e8284e2b017c200cb0928d505d510022
Rodancoci <rodancoci@gmail.com>
2017-06-25 02:19:50 -0400

    Fancy CSS, favicon, choice js as asset

---
+++ app/assets/javascripts/choice_button.js
@@ -0,0 +1,25 @@
+$(document).ready(function() {
+      var value_index
+      var max_fields      = 50;
+      var wrapper         = $(".values");
+      var add_button      = $(".add_form_field");
+    
+      var x = 1;
+      $(add_button).click(function(e){
+          e.preventDefault();
+          if(x < max_fields){
+              x++;
+              $(wrapper).append('<div class="field"><label>Choice</label><div class="extra_choice"><input required="required" type="text" name="bet[choices_attributes][][value]"><a href="#" class="delete choice">Delete</a></div></div>'); //add input box
+          }
+    else
+    {
+    alert("Can't add more choices")
+    }
+      });
+    
+      $(wrapper).on("click",".delete", function(e){
+          e.preventDefault(); $(this).parent('div').parent('div').remove(); x--;
+      })
+  });
+
+//Fields generables obtenidos de http://www.sanwebcorner.com/2017/02/dynamically-generate-form-fields-using.html
--- app/controllers/concerns/secured.rb
+++ app/controllers/concerns/secured.rb
@@ -8,6 +8,6 @@ module Secured
   ##
   # Verify if current_user is logged_in
   def logged_in?
-    redirect_to(root_path, notice: 'Unauthorized access!') unless current_user
+    redirect_to(root_path, alert: 'Unauthorized access!') unless current_user
   end
 end
